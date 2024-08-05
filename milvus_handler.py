68cb85b8c7c18fa63aeb80cc39ac947be507a221
woxane <mohsen.minavand1384@gmail.com>
2024-08-05 15:52:20 +0330

    feat(milvus_handler): implement MilvusHandler class with save_vectors and search_vectors function

---
+++ milvus_handler.py
@@ -0,0 +1,32 @@
+from pymilvus import MilvusClient
+
+
+
+class MilvusHandler:
+    def __init__(self, collection_name, dimensions, milvus_uri="http://localhost:1953"):
+        self.milvus_client = MilvusClient(milvus_uri)
+        self.collection_name = collection_name
+        self.dimensions = dimensions
+
+    def save_vectors(self, vectors, chunks):
+        if self.milvus_client.has_collection(collection_name=self.collection_name):
+            self.milvus_client.drop_collection(collection_name=self.collection_name)
+        self.milvus_client.create_collection(
+            collection_name=self.collection_name,
+            dimension=self.dimensions,
+        )
+
+        data = [
+            {"id": i, "vector": vectors[i], "text": chunks[i], "subject": "history"}
+            for i in range(len(vectors))
+        ]
+        self.milvus_client.insert(collection_name=self.collection_name, data=data)
+
+    def search_vectors(self, query_vector, top_k=3):
+        results = self.milvus_client.search(
+            collection_name=self.collection_name,
+            data=query_vector,
+            limit=top_k,
+            output_fields=["text", "subject"],
+        )
+        return results
