260718cdedac64be6136d0c928b799ed0c95e19f
woxane <mohsen.minavand1384@gmail.com>
2024-08-07 20:11:15 +0330

    feat(vectorizer): utilize get dimension from sentence transformer

--- vectorizer.py
+++ vectorizer.py
@@ -3,6 +3,7 @@ from sentence_transformers import SentenceTransformer
 class Vectorizer:
     def __init__(self, model_name):
         self.model = SentenceTransformer(model_name)
+        self.dimension = self.model.get_sentence_embedding_dimension()
 
     def vectorize(self, docs):
         return self.model.encode(docs)
