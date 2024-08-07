64f91785a8f000b0096cc618c0c0a0bbe1c12390
woxane <mohsen.minavand1384@gmail.com>
2024-08-07 19:30:54 +0330

    refactor(vectorizer): delete default value for model_name in class

--- vectorizer.py
+++ vectorizer.py
@@ -1,7 +1,7 @@
 from sentence_transformers import SentenceTransformer
 
 class Vectorizer:
-    def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
+    def __init__(self, model_name):
         self.model = SentenceTransformer(model_name)
 
     def vectorize(self, docs):
@@ -13,4 +13,4 @@ class Vectorizer:
             SentenceTransformer(model_name)
             return ""
         except Exception as e:
-            return e
+            return str(e)
