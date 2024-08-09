d2e3e4225e5fb5c16162c1cdc829399937a392e8
woxane <mohsen.minavand1384@gmail.com>
2024-08-09 16:55:07 +0330

    feat(milvus_handler): implement reset_db function to utilize it in main instead of constructor(streamlit problems*)

--- V-0.2/milvus_handler.py
+++ V-0.2/milvus_handler.py
@@ -5,42 +5,10 @@ from uuid import uuid4
 
 
 class MilvusHandler:
-    def __init__(self, collection_name, dimensions, milvus_uri, chunk_size=256):
+    def __init__(self, collection_name, dimensions, milvus_uri):
         self.milvus_client = MilvusClient(milvus_uri)
         self.collection_name = collection_name
         self.dimensions = dimensions
-
-
-        schema = MilvusClient.create_schema(
-            auto_id=False,
-            enable_dynamic_field=True,
-        )
-        schema.add_field(field_name="id", datatype=DataType.VARCHAR, max_length=64, is_primary=True)
-        schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=self.dimensions)
-        schema.add_field(field_name="text", datatype=DataType.VARCHAR, max_length=2*chunk_size)
-        schema.add_field(field_name="file_id", datatype=DataType.VARCHAR, max_length=64)
-
-        index_params = self.milvus_client.prepare_index_params()
-        index_params.add_index("id")
-        index_params.add_index(
-            field_name="vector",
-            index_type="IVF_FLAT",
-            metric_type="IP",
-            params={ "nlist": 128}
-        )
-        index_params.add_index("text")
-        index_params.add_index("file_id")
-
-        try:
-            self.milvus_client.create_collection(
-                collection_name=self.collection_name,
-                schema=schema,
-                index_params=index_params,
-            )
-
-        except:
-            if self.milvus_client.has_collection(collection_name=self.collection_name):
-                self.milvus_client.drop_collection(collection_name=self.collection_name)
 
     def save_vectors(self, vectors, chunks, file_id):
         data = [
@@ -75,3 +43,34 @@ class MilvusHandler:
             return ""
         except Exception as e:
             return str(e)
+
+
+    def reset_database(self, chunk_size=256):
+        schema = MilvusClient.create_schema(
+            auto_id=False,
+            enable_dynamic_field=True,
+        )
+        schema.add_field(field_name="id", datatype=DataType.VARCHAR, max_length=64, is_primary=True)
+        schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=self.dimensions)
+        schema.add_field(field_name="text", datatype=DataType.VARCHAR, max_length=2*chunk_size)
+        schema.add_field(field_name="file_id", datatype=DataType.VARCHAR, max_length=64)
+
+        index_params = self.milvus_client.prepare_index_params()
+        index_params.add_index("id")
+        index_params.add_index(
+            field_name="vector",
+            index_type="IVF_FLAT",
+            metric_type="IP",
+            params={ "nlist": 128}
+        )
+        index_params.add_index("text")
+        index_params.add_index("file_id")
+
+        if self.milvus_client.has_collection(collection_name=self.collection_name):
+            self.milvus_client.drop_collection(collection_name=self.collection_name)
+
+        self.milvus_client.create_collection(
+            collection_name=self.collection_name,
+            schema=schema,
+            index_params=index_params,
+        )
