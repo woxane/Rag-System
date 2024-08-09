4b458591996134994da6ef2ed3a9d91cd7f7be7e
woxane <mohsen.minavand1384@gmail.com>
2024-08-09 15:46:24 +0330

    feat(milvus_handler): update constructor to handle new schema for milvus db

--- V-0.2/milvus_handler.py
+++ V-0.2/milvus_handler.py
@@ -1,25 +1,48 @@
 from typing import Tuple, Type
 
-from pymilvus import MilvusClient
-
+from pymilvus import MilvusClient, DataType
+from uuid import uuid4
 
 
 class MilvusHandler:
-    def __init__(self, collection_name, dimensions, milvus_uri):
+    def __init__(self, collection_name, dimensions, milvus_uri, chunk_size=256):
         self.milvus_client = MilvusClient(milvus_uri)
         self.collection_name = collection_name
         self.dimensions = dimensions
+
+
+        schema = MilvusClient.create_schema(
+            auto_id=False,
+            enable_dynamic_field=True,
+        )
+        schema.add_field(field_name="id", datatype=DataType.VARCHAR, max_length=64, is_primary=True)
+        schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=self.dimensions)
+        schema.add_field(field_name="text", datatype=DataType.VARCHAR, max_length=2*chunk_size)
+        schema.add_field(field_name="file_id", datatype=DataType.VARCHAR, max_length=64)
+
+        index_params = self.milvus_client.prepare_index_params()
+        index_params.add_index("id")
+        index_params.add_index(
+            field_name="vector",
+            index_type="IVF_FLAT",
+            metric_type="IP",
+            params={ "nlist": 128}
+        )
+        index_params.add_index("text")
+        index_params.add_index("file_id")
+
 
         if self.milvus_client.has_collection(collection_name=self.collection_name):
             self.milvus_client.drop_collection(collection_name=self.collection_name)
         self.milvus_client.create_collection(
             collection_name=self.collection_name,
-            dimension=self.dimensions,
+            schema=schema,
+            index_params=index_params,
         )
 
     def save_vectors(self, vectors, chunks, file_id):
         data = [
-            {"id": i, "vector": vectors[i], "text": chunks[i], "subject": "history", "file_group": file_id}
+            {"id": str(uuid4()), "vector": vectors[i], "text": chunks[i], "file_id": file_id}
             for i in range(len(vectors))
         ]
 
@@ -31,7 +54,7 @@ class MilvusHandler:
             collection_name=self.collection_name,
             data=query_vector,
             limit=top_k,
-            output_fields=["text", "subject"],
+            output_fields=["text", "file_id"],
         )
         return results
 
@@ -39,7 +62,7 @@ class MilvusHandler:
     def delete_vectors(self, file_id):
         self.milvus_client.delete(
             collection_name=self.collection_name,
-            filter=f"file_group == {file_id}",
+            filter=f"file_id == '{file_id}'",
             )
 
 
