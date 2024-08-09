a65a1201943dc81db1e8c5bf2a7260a601835582
woxane <mohsen.minavand1384@gmail.com>
2024-08-09 11:06:44 +0330

    feat(chat_interface): use new template for streamlit and implement respond function

--- V-0.2/chat_interface.py
+++ V-0.2/chat_interface.py
@@ -12,33 +12,56 @@ class ChatInterface:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
-    def run(self):
-        st.set_page_config(page_title="Chat with AI", layout="wide")
-        st.title("Interactive Chat with Llama 3.1")
-        chat_placeholder = st.empty()
 
-        if 'messages' not in st.session_state:
+    def respond(self, user_input):
+        query_vector = self.vectorizer.vectorize([user_input])
+        search_results = self.milvus_handler.search_vectors(query_vector)
+        relevant_texts = [res['entity'].get("text") for res in search_results[0]]
+        context = "\n\n".join(relevant_texts)
+        prompt = self.chatbot.create_prompt(context, user_input)
+        st.session_state.messages.append({'role': 'user', 'content': user_input, 'rag_prompt': prompt})
+
+
+    def run(self):
+        st.title("PDF Helper")
+
+        # Initialize session state for pdf_texts and messages if not already present
+        if "files_id" not in st.session_state:
+            st.session_state.files_id= []
+
+        if "messages" not in st.session_state:
             st.session_state.messages = []
 
-        st.header("Upload File")
-        uploaded_file = st.file_uploader("Choose a file")
+        with st.sidebar:
+            st.header("Upload PDF Files")
+            uploaded_files = st.file_uploader("Choose PDF files", type="pdf", accept_multiple_files=True)
+            uploaded_ids = [file.file_id for file in uploaded_files]
 
-        if uploaded_file is not None:
-            chunks = self.document_processor.load_pdf(uploaded_file)
-            vectors = self.vectorizer.vectorize(chunks)
-            self.milvus_handler.save_vectors(vectors, chunks)
+            if uploaded_files:
+                for id in st.session_state.files_id:
+                    if id not in uploaded_ids:
+                        #This file has deleted
+                        #TODO: create delete rows with group_id
+                        st.session_state.files_id.remove(id)
 
-        st.header("Chat with AI")
+                for file in uploaded_files:
+                    if file.file_id not in st.session_state.files_id:
+                        #New file uploaded
+                        chunks = self.document_processor.load_pdf(file)
+                        vectors = self.vectorizer.vectorize(chunks)
+                        self.milvus_handler.save_vectors(vectors, chunks)
+
+                        st.success("PDF files uploaded successfully!")
+            else:
+                #Delete last remaining id
+                #TODO: create delete rows with group_id
+                st.session_state.files_id = []
 
         self.display_chat(st.session_state.messages)
 
+        # Accept user input
         if user_input := st.chat_input("Type your message here ..."):
-            query_vector = self.vectorizer.vectorize([user_input])
-            search_results = self.milvus_handler.search_vectors(query_vector)
-            relevant_texts = [res['entity'].get("text") for res in search_results[0]]
-            context = "\n\n".join(relevant_texts)
-            prompt = self.chatbot.create_prompt(context, user_input)
-            st.session_state.messages.append({'role': 'user', 'content': user_input , 'rag_prompt': prompt})
+            self.respond(user_input)
 
             with st.chat_message("user"):
                 st.markdown(user_input)
@@ -46,7 +69,8 @@ class ChatInterface:
             with st.chat_message("assistant"):
                 message_placeholder = st.empty()
                 full_response = ""
-                messages = [ {"role": message["role"], "content": message["rag_prompt"] if message["role"] == "user" else message["content"]} for message in st.session_state.messages ]
+                messages = [{"role": message["role"], "content": message["rag_prompt"]
+                if message["role"] == "user" else message["content"]} for message in st.session_state.messages]
                 completion = self.chatbot.get_response(messages)
 
                 for response in completion:
