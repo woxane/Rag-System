60d1f9379a5e63f4df536829360a8f63680aba4d
woxane <mohsen.minavand1384@gmail.com>
2024-08-12 12:29:14 +0330

    feat(env_manager): implement setup_env function

--- V-0.3/utils/env_manager.py
+++ V-0.3/utils/env_manager.py
@@ -1,4 +1,38 @@
+from dotenv import set_key
+
 
 class EnvManager:
     def __init__(self, dotenv_path: str = ".env"):
         self.path = dotenv_path
+
+    def setup_env(self):
+        # DocumentProcessor params part
+        chunk_size = size if (size := input("Enter chunk size for text split function (Enter for 256) : ")) else "256"
+        set_key(self.path, 'chunk_size', chunk_size)
+        chunk_overlap = overlap if (
+            overlap := input("Enter chunk overlap for text split function (Enter for 64) : ")) else "64"
+        set_key(self.path, 'chunk_overlap', chunk_overlap)
+
+        # Vectorizer param part
+        embedding_model_name = model if (model := input("Enter model name for word embedding"
+                                                        " (Enter for sentence-transformers/all-MiniLM-L6-v2) : ")) \
+            else "sentence-transformers/all-MiniLM-L6-v2"
+        set_key(self.path, 'embedding_model_name', embedding_model_name)
+
+        # MilvusHandler params part
+        collection_name = name if (name := input("Enter collection name for Milvus db (Enter for Test) : ")) else "Test"
+        set_key(self.path, 'collection_name', collection_name)
+        milvus_uri = uri if (uri := input("Enter your milvus uri (Enter for http://localhost:19530) : ")) \
+            else "http://localhost:19530"
+        set_key(self.path, 'milvus_uri', milvus_uri)
+
+        # Chatbot params part
+        openAI_base_url = url if (url := input("Enter open ai url to connect (Enter for http://localhost:1234/v1) : ")) \
+            else "http://localhost:1234/v1"
+        set_key(self.path, 'openAI_base_url', openAI_base_url)
+        openAI_api_key = key if (key := input("Enter your open ai api key (Enter for lm-studio) : ")) else "lm-studio"
+        set_key(self.path, 'openAI_api_key', openAI_api_key)
+        LLM_model_name = name if (name := input("Enter LLM model name "
+                                                "(Enter for lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF) : ")) \
+            else "lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF"
+        set_key(self.path, 'LLM_model_name', LLM_model_name)
