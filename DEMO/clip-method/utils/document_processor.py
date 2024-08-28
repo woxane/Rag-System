f108cfa07513ed1dd9cb47952c2915ebc32373e7
woxane <mohsen.minavand1384@gmail.com>
2024-08-28 20:28:11 +0330

    feat(document_processor): add getting images from a pdf feature

--- DEMO/clip-method/utils/document_processor.py
+++ DEMO/clip-method/utils/document_processor.py
@@ -1,7 +1,9 @@
 import fitz
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from typing import List
+from typing import List, Dict, Any
+from PIL import Image
+import io
 
 
 class DocumentProcessor:
@@ -20,9 +22,9 @@ class DocumentProcessor:
     def __repr__(self):
         return f"{self.__class__.__name__}(chunk_size={self.chunk_size!r})"
 
-    def load_pdf(self, file) -> List[str]:
+    def load_pdf(self, file) -> Dict[str, List[Any]]:
         """
-        Extracts text from a PDF file and splits it into chunks
+        Extracts text and images from a PDF file and splits it into chunks (just texts)
 
         This method using split the text using langchain.text_splitter.RecursiveCharacterTextSplitter to split the text.
 
@@ -30,15 +32,29 @@ class DocumentProcessor:
         file (file | streamlit file_uploader like objects): returning object of streamlit.file_uploader
 
         Returns:
-        List[str]: chunks that separated using RecursiveCharacterTextSplitter
+        Dict[str, List[Any]] = chunks: chunks that separated using RecursiveCharacterTextSplitter
+                               images: images that finded in the pdf file.
         """
         pdf_document = fitz.open(stream=file.read(), filetype="pdf")
         text = ""
+        pdf = {"chunks": [], "images": []}
+
         for page_num in range(len(pdf_document)):
             page = pdf_document.load_page(page_num)
             text += page.get_text()
+
+            images = page.get_images(full=True)
+
+            for image in images:
+                xref = image[0]
+                image_data = pdf_document.extract_image(xref)
+                image_bytes = image_data["image"]
+
+                pil_image = Image.open(io.BytesIO(image_bytes))
+                pdf['images'].append(pil_image)
+
         pdf_document.close()
 
         text = text.replace("\n", " ")
-        chunks = self.text_splitter.split_text(text)
-        return chunks
+        pdf['chunks'] = self.text_splitter.split_text(text)
+        return pdf 
